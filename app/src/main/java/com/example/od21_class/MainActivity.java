8981b93407b07884778a68db5276a14fa4821c69
gisacc54 <giftisacc54@gmail.com>
2024-06-12 01:01:41 +0300

    update

--- app/src/main/java/com/example/od21_class/MainActivity.java
+++ app/src/main/java/com/example/od21_class/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.od21_class;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +12,15 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private String[] languages = {"C", "C++", "Java", "Python", "JavaScript"};
+    private ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        listView = findViewById(R.id.list);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,languages);
+        listView.setAdapter(adapter);
     }
 }
