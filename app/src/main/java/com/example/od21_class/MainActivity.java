5ac38be95f00a1f2d6bed676568c6fe64185bb0c
gisacc54 <giftisacc54@gmail.com>
2024-06-21 22:56:20 +0300

    update

--- app/src/main/java/com/example/od21_class/MainActivity.java
+++ app/src/main/java/com/example/od21_class/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.od21_class;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
+//import com.example.myfirstapp.R;
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
